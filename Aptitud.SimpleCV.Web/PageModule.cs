34304e142c117fc26f2e65257297ff24bec240fd
Peter Qwarnstrom <peter.qwarnstrom@gmail.com>
2013-04-05 12:55:41 +0200

    Initial checkin

---
+++ Aptitud.SimpleCV.Web/PageModule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Aptitud.SimpleCV.Web {
+	public class PageModule : Nancy.NancyModule {
+
+		public PageModule() {
+			Get["/"] = _ => "Hello Stalker";
+		}
+	}
+}
