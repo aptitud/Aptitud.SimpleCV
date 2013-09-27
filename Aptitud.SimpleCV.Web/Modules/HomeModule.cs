18a0991b3cb8e501ca490d5bc70a951adae60908
Håkan Alexander <hakan.alexander@gmail.com>
2013-09-27 15:58:05 +0200

    Prettyfied first page

---
+++ Aptitud.SimpleCV.Web/Modules/HomeModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace Aptitud.SimpleCV.Web.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = parameters => View["Index"];
+        }
+    }
+}
