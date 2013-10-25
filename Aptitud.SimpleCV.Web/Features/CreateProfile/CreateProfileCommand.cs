a0533383b13af59b5929259668e224bc2889f28f
PeterQwarnstrom <peter.qwarnstrom@gmail.com>
2013-10-25 10:32:37 +0200

    Moved out CreateProfileCommand

---
+++ Aptitud.SimpleCV.Web/Features/CreateProfile/CreateProfileCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace Aptitud.SimpleCV.Web.Features.CreateProfile {
+
+    public class CreateProfileCommand {
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
--- Aptitud.SimpleCV.Web/Features/CreateProfile/CreateProfileModule.cs
+++ Aptitud.SimpleCV.Web/Features/CreateProfile/CreateProfileModule.cs
@@ -57,11 +57,4 @@ namespace Aptitud.SimpleCV.Web.Features.CreateProfile
                 };
         }
     }
-
-    public class CreateProfileCommand
-    {
-        public string EmailAddress { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-    }
 }
